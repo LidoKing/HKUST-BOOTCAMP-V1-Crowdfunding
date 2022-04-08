c5d29c7edef0f70f4aa6dcf3c4232336f4da15cd
LidoKing <kinglido203@gmail.com>
2022-04-09 01:15:40 +0800

    chore: changed visibility from public to external

--- contracts/DAIToken.sol
+++ contracts/DAIToken.sol
@@ -1,11 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity >=0.8.4 <0.9.0;
 
-interface IDAI {
-    function allowance(address arg1, address arg2) public returns (uint256);
+interface DAIToken {
+    function allowance(address arg1, address arg2) external returns (uint256);
 
     function transferFrom(
         address src,
         address dst,
         uint256 wad
-    ) public returns (bool);
+    ) external returns (bool);
 }
