00650bf0cfb8a897ccd8b1f46f1c83f4f5b88f8c
LidoKing <kinglido203@gmail.com>
2022-04-09 22:56:01 +0800

    feat: added contract types and signers

--- test/types.ts
+++ test/types.ts
@@ -1,11 +1,13 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import type { Fixture } from "ethereum-waffle";
 
-import type { Greeter } from "../src/types/Greeter";
+import type { Crowdfund } from "../src/types/contracts/Crowdfund";
+import type { TestToken } from "../src/types/contracts/TestToken";
 
 declare module "mocha" {
   export interface Context {
-    greeter: Greeter;
+    cf: Crowdfund;
+    tkn: TestToken;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
@@ -13,4 +15,8 @@ declare module "mocha" {
 
 export interface Signers {
   admin: SignerWithAddress;
+  signer1: SignerWithAddress;
+  signer2: SignerWithAddress;
+  signer3: SignerWithAddress;
+  signer4: SignerWithAddress;
 }
