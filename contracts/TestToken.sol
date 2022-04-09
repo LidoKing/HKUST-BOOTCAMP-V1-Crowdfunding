1cf6226350be9528443ec82d939da6b5c3169013
LidoKing <kinglido203@gmail.com>
2022-04-09 22:35:44 +0800

    fix: constructor param data location

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,9 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity >=0.8.4 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestToken is ERC20, UnitConverter {
-    constructor(address[] testers) ERC20("Test Token", "TKN") {
+contract TestToken is ERC20 {
+    constructor(address[] memory testers) ERC20("Test Token", "TKN") {
         uint256 mintAmount = 1000 * (10**18);
         for (uint256 i = 0; i < testers.length; i++) {
             _mint(testers[i], mintAmount);
