e3a0d3e2b5d1c98cadc0eeaf10561cf4735c09de
LidoKing <kinglido203@gmail.com>
2022-04-09 22:05:25 +0800

    chore: changed mint amount for each account

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,11 +1,10 @@
 pragma solidity >=0.8.4 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./UnitConverter.sol";
 
 contract TestToken is ERC20, UnitConverter {
     constructor(address[] testers) ERC20("Test Token", "TKN") {
-        uint256 mintAmount = to18Decimals(1000);
+        uint256 mintAmount = 1000 * (10**18);
         for (uint256 i = 0; i < testers.length; i++) {
             _mint(testers[i], mintAmount);
         }
