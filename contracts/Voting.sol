7aa4c03870ef1e159ac096888ff984cac33e24c6
LidoKing <kinglido203@gmail.com>
2022-05-05 01:11:30 +0800

    feat: voting condition checking and tracking

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,3 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity >=0.8.4 <0.9.0;
 
-contract Voting is Crowdfund {}
+import "./Initiation.sol";
+
+contract Voting is Initiation {
+    constructor(address _tokenAddress) Crowdfund(_tokenAddress) {}
+
+    mapping(uint256 => mapping(address => uint256)) power;
+    mapping(uint256 => mapping(address => bool)) voted;
+
+    modifier votable(uint256 _id, address _caller) {
+        require(hasFunded[_id][_caller] == true, "You did not fund this project.");
+        require(voted[_id][_caller] == false, "You have already voted.");
+        _;
+    }
+
+    modifier checkPhase(uint256 _id, uint256 _phase) {
+        State storage thisState = projectState[_id];
+        require(block.timestamp <= thisState.phaseDeadline[_phase], "Voting for this phase has ended.");
+        _;
+    }
+
+    function _register(uint256 _id, address _funder) private {
+        power[_id][_funder] = fundedAmount[_id][_funder];
+    }
+}
