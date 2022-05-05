cb2e066c87ebb8151700d002b1b7d0d7a5c19c2d
LidoKing <kinglido203@gmail.com>
2022-05-05 22:45:45 +0800

    feat: added voting type tracker

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -7,22 +7,22 @@ import "./Initiation.sol";
 contract Voting is Initiation {
     constructor(address _tokenAddress) Crowdfund(_tokenAddress) {}
 
+    // Track voting power for delegation
     mapping(uint256 => mapping(address => uint256)) power;
+    // Track voting types: 0 - For, 1 - Against, 2 - Abstain
+    mapping(uint256 => mapping(address => uint256)) stance;
+    // Track funder voting condition
     mapping(uint256 => mapping(address => bool)) voted;
 
-    modifier votable(uint256 _id, address _caller) {
-        require(hasFunded[_id][_caller] == true, "You did not fund this project.");
-        require(voted[_id][_caller] == false, "You have already voted.");
+    modifier votable(uint256 _projectId, address _caller) {
+        require(hasFunded[_projectId][_caller] == true, "You did not fund this project.");
+        require(voted[_projectId][_caller] == false, "You have already voted.");
         _;
     }
 
-    modifier checkPhase(uint256 _id, uint256 _phase) {
-        State storage thisState = projectState[_id];
+    modifier checkPhase(uint256 _projectId, uint256 _phase) {
+        State storage thisState = projectState[_projectId];
         require(block.timestamp <= thisState.phaseDeadline[_phase], "Voting for this phase has ended.");
         _;
-    }
-
-    function _register(uint256 _id, address _funder) private {
-        power[_id][_funder] = fundedAmount[_id][_funder];
     }
 }
