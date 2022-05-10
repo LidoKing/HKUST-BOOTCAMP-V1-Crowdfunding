74ea5338177d77dfe64ba77ecdb74e4278b57ca1
LidoKing <kinglido203@gmail.com>
2022-05-10 10:59:34 +0800

    feat: added wrapper contract for deployment

---
+++ contracts/Lidogogo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.4 <0.9.0;
+
+import "./Voting.sol";
+
+contract Lidogogo is Voting {
+    constructor(address _tokenAddress, address _aavePoolAddress) Voting(_tokenAddress, _aavePoolAddress) {}
+}
