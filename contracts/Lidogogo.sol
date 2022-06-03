a718f4b520a8614de8d26e3a8b4e710061b4b4c2
LidoKing <kinglido203@gmail.com>
2022-06-03 17:31:08 +0800

    feat: openzeppelin upgradeable proxy packages

--- contracts/Lidogogo.sol
+++ contracts/Lidogogo.sol
@@ -3,7 +3,15 @@
 pragma solidity >=0.8.4 <0.9.0;
 
 import "./Voting.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Lidogogo is Voting {
-    constructor(address _tokenAddress, address _aavePoolAddress) Voting(_tokenAddress, _aavePoolAddress) {}
+contract Lidogogo is Voting, Initializable {
+    function initialize(address _tokenAddress, address _aavePoolAddress) external initialize {
+        tknAddress = _tokenAddress;
+        tkn = IERC20(_tokenAddress);
+
+        aavePoolAddress = _poolContractAddress;
+        pool = IPool(_poolContractAddress);
+    }
+    //constructor(address _tokenAddress, address _aavePoolAddress) Voting(_tokenAddress, _aavePoolAddress) {}
 }
