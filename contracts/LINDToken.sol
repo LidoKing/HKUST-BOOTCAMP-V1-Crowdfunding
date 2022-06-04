adbe325f42d0166e3c26bde6d5b8d9d43fb23303
LidoKing <kinglido203@gmail.com>
2022-06-04 12:32:28 +0800

    chore: changed testing token distribution method (by owner)

--- contracts/LINDToken.sol
+++ contracts/LINDToken.sol
@@ -5,22 +5,20 @@ pragma solidity >=0.8.4 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LINDToken is ERC20 {
-    mapping(address => bool) isTester;
+    address owner;
 
-    constructor(address[5] memory _testers) ERC20("Lindogogo", "LIND") {
-        uint256 mintAmount = 1000 * (10**18);
-        for (uint256 i = 0; i < _testers.length; i++) {
-            isTester[_testers[i]] = true;
-            _mint(_testers[i], mintAmount);
-        }
+    constructor(address _owner) ERC20("Lindogogo", "LIND") {
+        owner = _owner;
+        uint256 mintAmount = 5000 * (10**18);
+        _mint(_owner, mintAmount);
     }
 
-    modifier onlyTesters() {
-        require(isTester[msg.sender] == true, "You are not qualified");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not qualified");
         _;
     }
 
-    function getTokens(uint256 _amount) external onlyTesters {
+    function getTokens(uint256 _amount) external onlyOwner {
         _mint(msg.sender, _amount);
     }
 }
