8f22f94442836f011a3f924b83fb886f15d8cc1a
LidoKing <kinglido203@gmail.com>
2022-06-04 23:33:49 +0800

    fix: renamed task name

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,7 +1,7 @@
 import "@nomiclabs/hardhat-web3";
 import { task } from "hardhat/config";
 
-task("balance", "Prints an account's balance", async (_, { web3 }) => {
+task("accounts", "Prints an account's balance", async (_, { web3 }) => {
   const accounts = await web3.eth.getAccounts();
 
   for (let i = 0; i < 5; i++) {
